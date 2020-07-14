4b1c3f424de05b02848982db1b86676a36a051dc
APTRiskyBiz <68022107+APTRiskyBiz@users.noreply.github.com>
2020-07-14 09:23:15 -0500

    Added a few configurable options

--- server/server.lua
+++ server/server.lua
@@ -1,54 +1,51 @@
 ESX = nil
+local TCE = TriggerClientEvent
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent('risky_selltonpc:dodeal')
 AddEventHandler('risky_selltonpc:dodeal', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
-	local item1 = xPlayer.getInventoryItem(Risky.Item1).count
-	local item2 = xPlayer.getInventoryItem(Risky.Item2).count
-	local item3 = xPlayer.getInventoryItem(Risky.Item3).count
-	local item4 = xPlayer.getInventoryItem(Risky.Item4).count
+	local item1 = xPlayer.getInventoryItem(Risky.I1.Item).count
+	local item2 = xPlayer.getInventoryItem(Risky.I2.Item).count
+	local item3 = xPlayer.getInventoryItem(Risky.I3.Item).count
+	local item4 = xPlayer.getInventoryItem(Risky.I4.Item).count
 	if item1 >= 1 and xPlayer ~= nil then
-		local item1price = Risky.Item1Price
-		local item1amount = Risky.Item1Count
+		local item1amount = Risky.I1.Count
 
-		local item1final = item1price * item1amount
+		local item1final = item1amount * Risky.I1.Price
 
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item1amount .. ' '.. Risky.Item1  ..' for $' .. item1final, length = 4000 })
-		xPlayer.removeInventoryItem(Risky.item1, item1amount)
-		xPlayer.addAccountMoney(Risky.Account, item1final)
+		TCE('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item1amount .. ' '.. Risky.I1.Item  ..' for $' .. item1final, length = 4000 })
+		xPlayer.removeInventoryItem(Risky.I1.Item, item1amount)
+		xPlayer.addAccountMoney(Risky.I1.Acc, item1final)
 
-	elseif item2 > 1 and xPlayer ~= nil then
-		local item2price = Risky.Item2Price	
-		local item2amount = Risky.Item2Count
+	elseif item2 > 1 and xPlayer ~= nil then	
+		local item2amount = Risky.I2.Count
 	
-		local item2final = item2price * item2amount
+		local item2final = item2amount * Risky.I2.Price
 	
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item2amount .. ' '.. Risky.Item2  ..' for $' .. item2final, length = 4000 })
-		xPlayer.removeInventoryItem(Risky.Item2, item2amount)
-		xPlayer.addAccountMoney(Risky.Account, item2final)
+		TCE('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item2amount .. ' '.. Risky.I2.Item  ..' for $' .. item2final, length = 4000 })
+		xPlayer.removeInventoryItem(Risky.I2.Item, item2amount)
+		xPlayer.addAccountMoney(Risky.I2.Acc, item2final)
 
 	elseif item3 > 1 and xPlayer ~= nil then
-		local item3price = Risky.Item3Price
-		local item3amount = Risky.Item3Count
+		local item3amount = Risky.I3.Count
 	
-		local item3final = item3price * item3amount
+		local item3final = item3amount * Risky.I3.Price
 	
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item3amount .. ' '.. Risky.Item3  ..' for $' .. item3final, length = 4000 })
-		xPlayer.removeInventoryItem(Risky.Item3, item3amount)
-		xPlayer.addAccountMoney(Risky.Account, item3final)
+		TCE('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item3amount .. ' '.. Risky.I3.Item  ..' for $' .. item3final, length = 4000 })
+		xPlayer.removeInventoryItem(Risky.I3.Item, item3amount)
+		xPlayer.addAccountMoney(Risky.I3.Acc, item3final)
 
 	elseif item4 > 1 and xPlayer ~= nil then
-		local item4price = Risky.Item4Price
-		local item4amount = Risky.Item4Count
+		local item4amount = Risky.I4.Count
 	
-		local item4final = item4price * item4amount
+		local item4final = item4amount * Risky.I4.Price
 	
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item4amount .. ' '.. Risky.Item4  ..' for $' .. item4final, length = 4000 })
-		xPlayer.removeInventoryItem(Risky.Item4, item4amount)
-		xPlayer.addAccountMoney(Risky.Account, item4final)
+		TCE('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. item4amount .. ' '.. Risky.I4.Item  ..' for $' .. item4final, length = 4000 })
+		xPlayer.removeInventoryItem(Risky.I4.Item, item4amount)
+		xPlayer.addAccountMoney(Risky.I4.Acc, item4final)
 	elseif item1 and item2 and item3 and item4 < 1 and xPlayer ~= nil then
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Come back when you are ready to become a REAL drug dealer, maybe next actually BRING drugs with you.', length = 4000 })
+		TCE('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Come back when you are ready to become a REAL drug dealer, maybe next actually BRING drugs with you.', length = 4000 })
 	end
 end)
 
@@ -56,20 +53,20 @@ RegisterNetEvent('countdrug')
 AddEventHandler('countdrug', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if xPlayer ~= nil then
-		local item1 = xPlayer.getInventoryItem(Risky.Item1).count
-		local item2 = xPlayer.getInventoryItem(Risky.Item2).count
-		local item3 = xPlayer.getInventoryItem(Risky.Item3).count
-		local item4 = xPlayer.getInventoryItem(Risky.Item4).count
+		local item1 = xPlayer.getInventoryItem(Risky.I1.Item).count
+		local item2 = xPlayer.getInventoryItem(Risky.I2.Item).count
+		local item3 = xPlayer.getInventoryItem(Risky.I3.Item).count
+		local item4 = xPlayer.getInventoryItem(Risky.I4.Item).count
 		if item1 >= 1 then
-			TriggerClientEvent("confirmcount", source, true)
+			TCE("confirmcount", source, true)
 		elseif item2 >= 1 then
-			TriggerClientEvent("confirmcount", source, true)
+			TCE("confirmcount", source, true)
 		elseif item3 >= 1 then
-			TriggerClientEvent("confirmcount", source, true)
+			TCE("confirmcount", source, true)
 		elseif item4 >= 1 then
-			TriggerClientEvent("confirmcount", source, true)
+			TCE("confirmcount", source, true)
 		else
-			TriggerClientEvent("confirmcount", source, false)
+			TCE("confirmcount", source, false)
 		end
 	end
 end)
